c12af56a5d27f554572ef0895bec63ce9f4507f7
Kelvin-Ben <kevoben14@gmail.com>
2023-04-28 16:49:04 +0300

    Create an app component as parent for all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Accordion from "./components/Accordion";
+import Count from "./components/Count";
+import Search from "./components/Search";
 
+const items = [
+  {
+    title: "What is React",
+    content: "React is front end development framework",
+  },
+  {
+    title: "Why React",
+    content: "React is the best framework",
+  },
+  {
+    title: "How to learn react?",
+    content: "You have to visit the official website for concepts",
+  },
+];
 const App = () => {
   return (
-    <div>Hello!</div>
-  )
-}
+    <div>
+      {/* <Accordion items={items} /> */}
+      <Search />
+      {/* <div><Count /></div> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
