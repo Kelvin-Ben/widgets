1542e9b877c1df50e653d9dc6eaa200da633c518
Kelvin-Ben <kevoben14@gmail.com>
2023-04-30 20:57:39 +0300

    Create useEffect function with a callback

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,35 +1,72 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: 'term',
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: "term",
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
         }
-      })
+      }, 500);
+      return () => {
+        clearTimeout(timeoutId);
+      };
     }
-    search();
-  }, [term])
-  
+  }, [term]);
+
   return (
     <div>
-      <div className='ui form'>
-        <div className='field'>
+      <div className="ui form">
+        <div className="field">
           <label>Enter Search term</label>
-          <input className='input' type='text' value={term}
-          onChange={e => setTerm(e.target.value)}/>
+          <input
+            className="input"
+            type="text"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
